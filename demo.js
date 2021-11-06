a9c2aa8c348c2bf061282e65c08309eff5411f29
Rajesh Bhusal <tipstricks118@gmail.com>
2021-11-06 19:52:06 +0545

    First commit

---
+++ demo.js
@@ -0,0 +1,10 @@
+const person=[
+    {name:"Rajesh Bhusal", age:19},
+    {name:"Rajesh Bhusal", age:19},
+    {name:"Rajesh Bhusal", age:19},
+]
+
+person.forEach(item=>{
+    console.log(item)
+})
+
---
+++ index.js
@@ -0,0 +1,59 @@
+const axios= require('axios');
+const express= require('express');
+const cheerio= require('cheerio');
+
+async function getPriceFeed(){
+    try{
+
+        const siteUrl='https://merolagani.com/LatestMarket.aspx';
+        const {data}= await axios.get(siteUrl);
+        
+        const $=cheerio.load(data);
+        const elemSelector= "#ctl00_ContentPlaceHolder1_LiveTrading > table > tbody > tr"
+        const selectedData= $(elemSelector);
+        
+        const keys=[
+            "symbol", "ltp", "% change", "high", "low", "open", "qty"
+        ]
+
+        const nepseData= []
+        
+        selectedData.each((index,valueElement)=>{
+            const nepseObjData= {};
+            
+                const eachElement= $(valueElement).children();
+                eachElement.each((child, childElem)=>{
+                    const tdValue= $(childElem).text();
+                    if(tdValue){
+                        nepseObjData[keys[child]] =tdValue;   
+                    }
+                })
+                
+            nepseData.push(nepseObjData)
+            
+
+            
+        })
+        return(nepseData)
+    
+    }catch(err){
+        console.log(err);
+    }
+}
+
+const app= express();
+
+app.get("/api/price", async (req,res)=>{
+    try{
+        const price= await getPriceFeed();
+        return res.status(500).json({
+            result:price,
+        })
+    }catch(err){
+        console.log(err)
+    }
+})
+
+app.listen(80, ()=>{
+    console.log("Server is running")
+})
